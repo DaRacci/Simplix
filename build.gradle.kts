22814174262dcff68c5cdc5e4ceefcb236a86807
DaRacci <racci@racci.dev>
2022-12-02 21:26:46 +1100

    chore: Add cog and scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription.Permission.Default
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder
 
+// Workaround for (https://youtrack.jetbrains.com/issue/KTIJ-19369)
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.minix.kotlin)
     alias(libs.plugins.minix.copyjar)
@@ -15,7 +17,7 @@ bukkit {
     this.apiVersion = "1.19"
     this.depend = listOf("Minix")
     this.main = "dev.racci.simplix.Simplix"
-    this.load = PluginLoadOrder.STARTUP
+    this.load = PluginLoadOrder.POSTWORLD
 
     this.permissions {
         this.register("simplix.rename") {
@@ -36,6 +38,10 @@ tasks {
         dependsOn(shadowJar)
         findByName("copyJar")?.let { dependsOn(it) }
     }
+}
+
+kotlin {
+    explicitApiWarning()
 }
 
 dependencies {
